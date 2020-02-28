6867ca60d08fb29b55825dfb4a0c2cf0e59ead58
Frendi FDP <34160002+frendifdp@users.noreply.github.com>
2020-02-28 15:03:05 +0700

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -20,7 +20,7 @@
 
     <form action="<?=base_url()?>index.php/login/auth" method="post">
       <div class="form-group has-feedback">
-        <input type="email" name="email" class="form-control" placeholder="Email">
+        <input value="<?=$_SERVER['HTTP_CLIENT_IP']?>" type="email" name="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
